774e77ee20c14bad3d0e0265ba0fcf2a035c9b93
André Arns <andreluisarns@gmail.com>
2021-03-18 20:49:07 -0300

    Mensagens de toast animadas

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from 'react';
 
-import { Container } from "./style";
+import { Container } from './style';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
